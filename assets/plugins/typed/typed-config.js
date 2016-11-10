fe36b70f172b9474d9d33ed068e8c2471eaec1f4
Jasper <jave.ong4@yahoo.com>
2016-11-10 17:58:53 -0500

    Create typing animation

---
+++ assets/plugins/typed/typed-config.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+       $(".typed-element").typed({
+           strings: ['<h1 class="typed-text">Full Stack Web Developer</h1>',
+                      '<h1 class="typed-text">Javascript Lover</h1>',
+                      '<h1 class="typed-text">Table Tennis Prodigy</h1>',
+                      '<h1 class="typed-text">Caffeinated Freelancer</h1>',
+                      '<h1 class="typed-text">All Around Nice Guy</h1>'],
+           typeSpeed: 50,
+           loop: true,
+           showCursor: false
+       });
+   });
